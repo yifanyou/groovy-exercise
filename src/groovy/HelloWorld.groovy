d77e8bee6300c27c0adc8373a91287d206ca1032
You YiFan <yyfan2016@gmail.com>
2017-05-09 20:50:23 +0800

    hello world

---
+++ src/groovy/HelloWorld.groovy
@@ -0,0 +1,14 @@
+package groovy
+
+/**
+ * 创建者:   youyifan
+ * 创建时间: 2017/5/9 20:44
+ * 功能描述:
+ * 修改历史:
+ */
+class HelloWorld {
+    static void main(args){
+        println "Hello World!";
+    }
+}
+
